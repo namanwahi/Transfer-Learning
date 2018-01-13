f1bc5820af91fa9d0ad9aeec7bffe4fdf31c405c
nw316 <nw316@ic.ac.uk>
2018-01-13 21:50:16 +0000

    added whole test set

--- CIFAR10_utils.py
+++ CIFAR10_utils.py
@@ -7,6 +7,8 @@ transform = transforms.Compose(
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+whole_trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+whole_testset =  torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True)
