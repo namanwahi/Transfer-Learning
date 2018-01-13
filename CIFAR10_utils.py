3f6fe03f1976b44e71df1bf2aa91709c0cbab4ac
nw316 <nw316@ic.ac.uk>
2018-01-13 21:45:59 +0000

    added dataloader for cifar 10

---
+++ CIFAR10_utils.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.RandomHorizontalFlip(),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True)
