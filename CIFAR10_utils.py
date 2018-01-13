046accad6ed3f1597ad5d2f2e4ba5511242ab1e1
nw316 <nw316@ic.ac.uk>
2018-01-13 22:33:45 +0000

    implemented methods to select the subset of CIFAR10 set

--- CIFAR10_utils.py
+++ CIFAR10_utils.py
@@ -2,6 +2,18 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
+def CIFAR10_whole():
+    return whole_trainset, whole_testset
+
+def _CIFAR10_subset(class_indices):
+    trainset = [x for x in whole_trainset if x[1] in class_indices]
+    testset = [x for x in whole_testset if x[1] in class_indices]
+    return trainset, testset
+
+def CIFAR_animals():
+    return _CIFAAR10_subset([2, 3, 4, 5, 6, 7])
+    
+
 transform = transforms.Compose(
     [transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
@@ -11,4 +23,5 @@ whole_trainset = torchvision.datasets.CIFAR10(root='./data', train=True, downloa
 
 whole_testset =  torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=256, shuffle=True)
+train, test = _CIFAR10_subset([1,2,3])
+
