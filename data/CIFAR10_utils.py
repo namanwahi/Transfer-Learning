36c3633902f1de062b20236e0d4c0f2e1257f9c8
namanwahi <namanwahi@yahoo.co.uk>
2018-01-27 11:57:47 +0000

    moved the transformation of the dataset into the function to load it

--- data/CIFAR10_utils.py
+++ data/CIFAR10_utils.py
@@ -4,21 +4,11 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
 def CIFAR10_whole():
-    whole_trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
-    whole_testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
-    return whole_trainset, whole_testset
-
-def CIFAR10_subset_sampler(class_indices):
-    return SubsetRandomSampler(class_indices)
-
-def CIFAR10_animal_sampler():
-    return CIFAR10_subset_sampler([2, 3, 4, 5, 6, 7])
-    
-
-transform = transforms.Compose(
+    transform = transforms.Compose(
     [transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-
+    whole_trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
+    whole_testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
+    return whole_trainset, whole_testset
 
