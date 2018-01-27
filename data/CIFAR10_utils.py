c68d8ec594da6e1296c2f7c2c10c1284daaf777e
namanwahi <namanwahi@yahoo.co.uk>
2018-01-27 11:51:49 +0000

    added the loading of the whole dataset into a function

--- data/CIFAR10_utils.py
+++ data/CIFAR10_utils.py
@@ -4,6 +4,8 @@ import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
 def CIFAR10_whole():
+    whole_trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
+    whole_testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
     return whole_trainset, whole_testset
 
 def CIFAR10_subset_sampler(class_indices):
@@ -18,8 +20,5 @@ transform = transforms.Compose(
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-whole_trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
-
-whole_testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
 
 
