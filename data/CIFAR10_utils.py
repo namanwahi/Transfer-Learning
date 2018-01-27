005dda3e8c4d18ba1a103608d9fe41ff7d6f247d
namanwahi <namanwahi@yahoo.co.uk>
2018-01-27 12:07:58 +0000

    removed test for downloading the dataset

--- data/CIFAR10_utils.py
+++ data/CIFAR10_utils.py
@@ -3,12 +3,11 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 
-def CIFAR10_whole():
+def get_CIFAR10_dataset():
     transform = transforms.Compose(
     [transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    whole_trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
-    whole_testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
-    return whole_trainset, whole_testset
-
+    trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
+    testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
+    return trainset, testset
