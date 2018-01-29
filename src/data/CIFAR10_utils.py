2154b6e19150b42b5655bfefcefe7aaebb9c8d82
namanwahi <namanwahi@yahoo.co.uk>
2018-01-29 10:54:42 +0000

    made function compatable for resnet input sizes

--- src/data/CIFAR10_utils.py
+++ src/data/CIFAR10_utils.py
@@ -3,15 +3,19 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.utils.data import DataLoader
+import os
+
+path = os.path.abspath(__file__)
+dir_path = os.path.dirname(path)
 
 def _get_dataset():
     transform = transforms.Compose(
-    [transforms.Resize(256),
+    [transforms.Resize(224),
      transforms.RandomHorizontalFlip(),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
-    testset =  torchvision.datasets.CIFAR10(root='./', train=False, download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root=dir_path, train=True, download=True, transform=transform)
+    testset =  torchvision.datasets.CIFAR10(root=dir_path, train=False, download=True, transform=transform)
     return trainset, testset
 
 def _get_classes():
@@ -21,8 +25,8 @@ def get_dataloader(train=True):
     animal_indices = [2, 3, 4, 5, 6, 7]
     #animal_sampler = SubsetRandomSampler(animal_indices)
     if train:
-        return DataLoader(_get_dataset()[0], batch_size=256)
+        return DataLoader(_get_dataset()[0], batch_size=4)
     else:
-        return DataLoader(_get_dataset()[1], batch_size=256)
+        return DataLoader(_get_dataset()[1], batch_size=4)
         
 
