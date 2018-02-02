f5c6c8657c48b11b943cf0f095bf9c21874f8123
namanwahi <namanwahi@yahoo.co.uk>
2018-02-02 00:18:54 +0000

    added utilities file for pretained models for transfer learning

---
+++ src/training/transfer_learning_models.py
@@ -0,0 +1,18 @@
+from torchvision import  models
+from data.CIFAR10_utils import get_dataloader
+from training.training_utils import train_model
+import torch.nn as nn
+
+imagenet_classes = 1000
+
+def get_resnet18_model(class_no=imagenet_classes, fixed_feature_extractor=True):
+    model = models.resnet18(pretrained=True)
+
+    #freeze all the weights of the network
+    if fixed_feature_extractor:
+        for param in model.parameters():
+           param.requires_grad = False
+
+    #replace the final layer of the neural network
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, class_no)
